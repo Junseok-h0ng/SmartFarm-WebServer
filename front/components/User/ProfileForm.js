8197305fbf4550d0e28db90f9ba8bdd53fcf4480
junseok <wnstjr6467@gmail.com>
2021-04-02 23:27:02 +0900

    Add: Profile UserData

--- front/components/User/ProfileForm.js
+++ front/components/User/ProfileForm.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {Avatar,Button,Card} from 'antd';
+import {useSelector} from 'react-redux';
 
 function ProfileForm() {
+    const user = useSelector(state => state.user);
     return (
         <div>
             <Card>
                 <Button style={{float:'right'}}>제어창</Button>
                 <Card.Meta
-                    avatar={<Avatar>{'U'}</Avatar>}        
-                    title={'username'}
-                    description={'email@naver.com'}
+                    avatar={<Avatar>{user.data.name[0]}</Avatar>}        
+                    title={user.data.name}
+                    description={user.data.email}
                 />
             </Card>
         </div>
